c27103f947d80cf8623ec4a468ae4123a9e58443
Woody <colorguitar@hotmail.com>
2016-11-14 22:37:15 +0800

    添加了首页的tableViewController

--- Remember/AppDelegate.swift
+++ Remember/AppDelegate.swift
@@ -17,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        self.window!.rootViewController = UINavigationController.init(rootViewController: ViewController(style: UITableViewStyle.grouped));
+        
         return true
     }
 
--- Remember/ViewController.swift
+++ Remember/ViewController.swift
@@ -8,18 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.title = "丁丁记事"
+        self.view.backgroundColor = UIColor.white
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "关于", style: .plain, target: self, action: #selector(ViewController.pushToAboutPage(_:)))
+        
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    func pushToAboutPage(_ sender:UIBarButtonItem){
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
