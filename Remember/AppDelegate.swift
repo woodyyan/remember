d753ef5bd6d3582692b7dfec6686565d0ccae07e
Woody <colorguitar@hotmail.com>
2017-01-13 14:16:13 +0800

    添加了Firebase分析。

--- Remember/AppDelegate.swift
+++ Remember/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Use Firebase library to configure APIs
+        FIRApp.configure()
         
         self.window!.rootViewController = UINavigationController.init(rootViewController: HomeViewController());
         
