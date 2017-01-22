bd44a1ed7759560369d036c4a4710ce5d8a2e7d4
Woody <colorguitar@hotmail.com>
2017-01-22 15:43:18 +0800

    修改了提示文字。

--- Remember/Presenter/StringConstants.swift
+++ Remember/Presenter/StringConstants.swift
@@ -9,6 +9,6 @@
 import Foundation
 
 class StringConstants {
-    static let sampleThing = "比如：\n三脚架在右边床头柜里\n女朋友的生日是2月14日\n妈妈最喜欢吃的菜是土豆丝\n毕业证书在衣柜底部的抽屉里\n爸爸的烟藏在厨房第三个柜子里面"
+    static let sampleThing = "比如：\n三脚架在主卧床头柜里\n女朋友的生日是2月14日\n妈妈最喜欢吃的菜是土豆丝\n毕业证书在衣柜底部的抽屉里\n爸爸的烟藏在厨房第三个柜子里面"
     static let description = "丁丁记事是一个帮助你记住平时容易忘记的小事的助手，你可以非常快速而简单的记录一切容易忘记的小事，当你忘记它们的时候，可以很方便的从这里找到它们。"
 }
