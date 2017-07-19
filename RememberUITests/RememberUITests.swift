eb594cb400d3d6ecd6314239cb8973475771cd74
JessieDing <nathalie.ding@gmail.com>
2017-07-19 19:42:44 +0800

    Added some ui test code.

--- RememberUITests/RememberUITests.swift
+++ RememberUITests/RememberUITests.swift
@@ -28,9 +28,24 @@ class RememberUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    func testAllItemsExistInAboutPage() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+//        let app = XCUIApplication()
+//        app.navigationBars["丁丁记事"].buttons["about"].tap()
+//        
+//        let tablesQuery = app.tables
+//        var isExists = tablesQuery.staticTexts["记住你容易忘记的小事"].exists
+//        XCTAssert(isExists)
+//        
+//        isExists = tablesQuery.staticTexts["版本号：V1.9"].exists
+//        XCTAssert(isExists)
+//        isExists = tablesQuery.staticTexts["丁丁记事是一个帮助你记住平时容易忘记的小事的轻量级备忘录，你可以非常快速而简单的记录一切容易忘记的小事，当你忘记它们的时候，可以很方便的从这里找到它们。"].exists
+//        XCTAssert(isExists)
+//        isExists = app.buttons["反馈"].exists
+//        XCTAssert(isExists)
+//        isExists = app.buttons["微博"].exists
+//        XCTAssert(isExists)
     }
-    
 }
